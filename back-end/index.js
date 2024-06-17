504c0d219c0f93238ae441987dbd3a1bf785850e
nguyenhaugiang <giangb2014563@student.ctu.edu.vn>
2024-06-18 00:26:11 +0700

    config vercel

--- back-end/index.js
+++ back-end/index.js
@@ -51,20 +51,47 @@ app.use((err, req, res, next) => {
     });
 });
 
-async function startServer() {
-    try {
-        await MongoDB.connect(config.db.uri);
-        console.log('Connected to the DB')
-        const PORT = config.app.port;
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        })
-    } catch (error) {
-        console.log("Can not connect to the DB !!!", error);
-        process.exit();
-    }
-}
 
-startServer();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(config.db.uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("random_questions").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// async function startServer() {
+//     try {
+//         await MongoDB.connect(config.db.uri);
+//         console.log('Connected to the DB')
+//         const PORT = config.app.port;
+//         app.listen(PORT, () => {
+//             console.log(`Server is running on port ${PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("Can not connect to the DB !!!", error);
+//         process.exit();
+//     }
+// }
+
+// startServer();
 
 module.exports = app;
