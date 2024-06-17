b561d1ac81177a4d7ef28c515b7c600b798a9d79
nguyenhaugiang <giangb2014563@student.ctu.edu.vn>
2024-06-17 22:08:04 +0700

    config vercel

--- back-end/index.js
+++ back-end/index.js
@@ -9,8 +9,19 @@ const app = express();
 const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 
+const allowedOrigins = ['https://full-project-v1.vercel.app', 'https://full-project-six.vercel.app'];
+
 app.use(cors({
-    origin: ['https://full-project-v1.vercel.app'],
+    origin: function(origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 app.use(express.json());
