bc58fdc2b393f2eec0158c4d289ce2c760664b26
nguyenhaugiang <giangb2014563@student.ctu.edu.vn>
2024-06-18 14:40:46 +0700

    config vercel

--- back-end/index.js
+++ back-end/index.js
@@ -51,25 +51,32 @@ app.use((err, req, res, next) => {
     });
 });
 
-const uri = config.db.uri;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nguyenhaugiang0702:9JpeCZ254uarSPt3@cluster0.vzvlyq3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-async function startServer() {
-    try {
-        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-        console.log('Connected to the DB');
-        const PORT = process.env.PORT || 3000;
-        app.locals.db = client.db('random_questions'); // Set the db on app locals
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.log("Cannot connect to the DB !!!", error);
-        process.exit();
-    }
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("random_questions").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+    console.log('Error');
+  }
 }
+run();
 
-startServer();
 
 module.exports = app;
